e4475c543923a3f89f81642c5da96bc6c9dade00
CodeJuuun <Chomseng.a@gmail.com>
2024-11-27 15:39:24 -0500

    added test case to ensure original array is not modified

--- tail.js
+++ tail.js
@@ -13,15 +13,22 @@ const tail  = function(arr) {
   }
   return arr.slice(1);
 };
+
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
 const result = tail(['Hello', 'World', 'Labs']);
 assertEqual(result.length, 2);
 
 //checks if array only has one element
 const oneElement = tail(['Hello']);
-console.log(oneElement)
 assertEqual(oneElement.length, 0);
 
 
 // checks if array is empty
 const emptyArr = tail([]);
 assertEqual(emptyArr.length, 0);
+
