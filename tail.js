111f189004951c2fdcefd7e702a32a9fc8e53e0b
CodeJuuun <Chomseng.a@gmail.com>
2024-11-27 15:29:18 -0500

    submitting for review, will revise code

--- tail.js
+++ tail.js
@@ -6,33 +6,21 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-
-// -----------------------
-// created new function to compare arrays
-// const checkArrElements = function(arr1, arr2){
-//   if (arr1.length === arr2.length) {
-//     for (let i = 0; i < arr1.length; i++) {
-//       if (arr1[i] === arr2[i]) {
-//        console.log(`😀😀😀 Assertion Passed: ${arr1} === ${arr2}`)
-//       }
-//     }
-//   } else {
-//     console.log(`🤢🤢🤢 Assertion Failed: ${arr1} !== ${arr2}`)
-//   }
-// }
-
-const tail = function(tailEndArr) {
-  return tailEndArr.slice(1);
+// modified to check if the length of the array is less than or 1
+const tail  = function(arr) {
+  if (arr.length <= 1) {
+    return [];
+  }
+  return arr.slice(1);
 };
+const result = tail(['Hello', 'World', 'Labs']);
+assertEqual(result.length, 2);
 
-// const result = tail(["Hello", "Lighthouse", "Labs"]); // should return ["Lighthouse", "Labs"]
-// checkArrElements(result, ["Lighthouse", "Labs"]); // => will always fail!
+//checks if array only has one element
+const oneElement = ['Hello'];
+assertEqual(oneElement.length, 1);
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-const onlyOneElement = ["one"];
+// checks if array is empty
 const emptyArr = [];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-assertEqual(tail(onlyOneElement), []);
-assertEqual(tail(emptyArr), []);
+assertEqual(emptyArr.length, 0);
