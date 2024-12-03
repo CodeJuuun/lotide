33a3a619f7130fe5509779f98df04ec8e0a3affb
CodeJuuun <Chomseng.a@gmail.com>
2024-12-03 15:37:43 -0500

    used eslint

--- countOnly.js
+++ countOnly.js
@@ -24,6 +24,7 @@ const countOnly = (allItems, itemsToCount) => {
   for (const item of allItems) {
     //checks if BOTH the object key AND the value exists and if it does, increase the count, meaning if the value is not a falsey value (greater than 0)
     if (itemsToCount[item]) {
+      console.log(itemsToCount[item]);
       if (results[item]) {
         results[item] += 1;
       } else {
