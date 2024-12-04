630ae28c325c529ab40e7aa75507d594f92004b3
CodeJuuun <Chomseng.a@gmail.com>
2024-12-04 16:56:14 -0500

    modified conditional check on line 38 to to return the false so it can continue to check other values

--- eqObjects.js
+++ eqObjects.js
@@ -35,13 +35,11 @@ const eqObjects = function(obj1, obj2) {
 
   for (let key of obj1keys) {
     if (Array.isArray(obj1[key]) && (Array.isArray(obj2[key]))) { // if both conditions pass and returns true and both evaluate to arrays---
-      if (eqArrays(obj1[key], obj2[key])) { // use function to check the comparison of both object's key values and if they match, return true
-        return true;
-      }
-    } else {
-      if (obj1[key] !== obj2[key]) {
+      if (!eqArrays(obj1[key], obj2[key])) { // use function to check the comparison of both object's key values if they are not matching
         return false;
       }
+    } else if (obj1[key] !== obj2[key]) { //compares values directly if primitive
+      return false;
     }
   }
   return true;
