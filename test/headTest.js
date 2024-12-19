a3eca932d0a46deba8f5ea898a9961afece9e3c7
CodeJuuun <Chomseng.a@gmail.com>
2024-12-19 16:46:27 -0500

    removed commments

--- test/headTest.js
+++ test/headTest.js
@@ -31,13 +31,3 @@ describe('#head', () => {
     assert.strictEqual(head(['string', 10, true, ['nestedArr, nestedArr2']]), 'string');
   });
 });
-
-
-
-
-// // TEST CODE
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([100]), 100);  // An array with only one element should still yield that one element as its head
-// assertEqual(head([]), undefined);  // An empty array should yield undefined as its head
-// assertEqual(head(["string", 10, true, ["nestedArr, nestedArr2"]]), "string"); // An array with different data types
