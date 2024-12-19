486356794f38744ce39ccfcf5ca1a75b9251ebe2
CodeJuuun <Chomseng.a@gmail.com>
2024-12-18 22:35:18 -0500

    refactored tail test code into separate test file and ran successfully

--- tail.js
+++ tail.js
@@ -15,20 +15,5 @@ const tail  = function(arr) {
 };
 
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
 
-
-const result = tail(['Hello', 'World', 'Labs']);
-assertEqual(result.length, 2);
-
-//checks if array only has one element
-const oneElement = tail(['Hello']);
-assertEqual(oneElement.length, 0);
-
-
-// checks if array is empty
-const emptyArr = tail([]);
-assertEqual(emptyArr.length, 0);
-
+module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,18 @@
+const assertEqual = require("../assertEqual")
+const tail  = require("../tail")
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+const result = tail(['Hello', 'World', 'Labs']);
+assertEqual(result.length, 2);
+
+//checks if array only has one element
+const oneElement = tail(['Hello']);
+assertEqual(oneElement.length, 0);
+
+
+// checks if array is empty
+const emptyArr = tail([]);
+assertEqual(emptyArr.length, 0);
