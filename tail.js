58465442cf38ff7999b3dda34e8361b3bbbb7000
CodeJuuun <Chomseng.a@gmail.com>
2024-12-18 22:39:45 -0500

    removed assertEquals function

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀😀😀 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤢🤢🤢 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // modified to check if the length of the array is less than or 1
 const tail  = function(arr) {
   if (arr.length <= 1) {
