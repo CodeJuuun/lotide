80da6a1796f0680c04dade990e83322efacf22fb
CodeJuuun <Chomseng.a@gmail.com>
2024-12-20 15:28:59 -0500

    added map function

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ const assertArraysEqual  = require("./assertArraysEqual");
 const assertEqual       = require("./assertEqual");
 const assertObjectsEqual = require("./assertObjectsEqual");
 const countLetters      = require("./countLetters");
-const countOnly         = require("./countOnly")
+const countOnly         = require("./countOnly");
 const eqArrays          = require("./eqArrays");
 const eqObjects         = require("./eqObjects");
 const findKey           = require("./findKey");
-const findKeyByValue    = require("./findKeyByValue")
+const findKeyByValue    = require("./findKeyByValue");
 const letterPositions   = require("./letterPositions");
+const map               = require("./map");
 const takeUntil         = require("./takeUntil");
 const without           = require("./without");
 
@@ -29,6 +30,7 @@ module.exports = {
   findKey:            findKey,
   findKeyByValue:     findKeyByValue,
   letterPositions:    letterPositions,
+  map:                map,
   takeUntil:          takeUntil,
   without:            without
-}
+};
