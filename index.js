436c129035d58abc10bd26cbb2934149929eff53
CodeJuuun <Chomseng.a@gmail.com>
2024-12-20 15:26:21 -0500

    revised function typo and deleted functions not listed in requirements

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const head              = require("./head");
 const tail              = require("./tail");
 const middle            = require("./middle");
-const assertArrayEqual  = require("./assertArraysEqual");
+const assertArraysEqual  = require("./assertArraysEqual");
 const assertEqual       = require("./assertEqual");
-const assertObjectEqual = require("./assertObjectsEqual");
+const assertObjectsEqual = require("./assertObjectsEqual");
 const countLetters      = require("./countLetters");
 const countOnly         = require("./countOnly")
 const eqArrays          = require("./eqArrays");
 const eqObjects         = require("./eqObjects");
 const findKey           = require("./findKey");
 const findKeyByValue    = require("./findKeyByValue")
-const flatten           = require("./flatten");
 const letterPositions   = require("./letterPositions");
-const map               = require("./map");
 const takeUntil         = require("./takeUntil");
 const without           = require("./without");
 
@@ -21,18 +19,16 @@ module.exports = {
   head:               head,
   tail:               tail,
   middle:             middle,
-  assertArrayEqual:   assertArrayEqual,
+  assertArraysEqual:   assertArraysEqual,
   assertEqual:        assertEqual,
-  assertObjectEqual:  assertObjectEqual,
+  assertObjectsEqual:  assertObjectsEqual,
   countLetters:       countLetters,
   countOnly:          countOnly,
   eqArrays:           eqArrays,
   eqObjects:          eqObjects,
   findKey:            findKey,
   findKeyByValue:     findKeyByValue,
-  flatten:            flatten,
   letterPositions:    letterPositions,
-  map:                map,
   takeUntil:          takeUntil,
   without:            without
 }
